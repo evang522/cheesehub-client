07c66b70b9f8bb4043bf9192bd14d31bf31dc780
Evan Garrett <evang522@gmail.com>
2018-03-09 15:56:13 -0500

    Finish reducer

--- src/reducers/cheese.js
+++ src/reducers/cheese.js
@@ -8,5 +8,14 @@ const initialState =    {
 }
 
 const reducer = (state=initialState, action) => {
-  if (action.type= )
+  if (action.type=== FETCH_CHEESES_REQUEST) {
+    return Object.assign({},state, {loading:true});
+
+  }
+  if (action.type=== FETCH_CHEESES_ERROR) {
+    return Object.assign({}, state, {error:action.err});
+  }
+  if (action.type=== FETCH_CHEESES_SUCCESS) {
+    return Object.assign({}, state, {cheeses: action.cheeses});
+  }
 };
